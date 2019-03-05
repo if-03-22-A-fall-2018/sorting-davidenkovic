ceb82e6d100abda83725d291b05a582fb9bbb5f0
Ignjatovic David <Ignjatovicdavid1@gmail.com>
2019-03-05 11:04:01 +0100

    insert and bubble is ok

--- sorting_algorithms.cpp
+++ sorting_algorithms.cpp
@@ -18,9 +18,27 @@ void 	init_random (int *array, unsigned long length){
 }
 
 void 	bubble_sort (int *array, unsigned long length){
-
+   int i, j,tmp;
+   for (i = 1; i < length ; i++){
+      for (j = 0; j < length - i ; j++){
+          if (array[j] > array[j+1]){
+              tmp = array[j];
+              array[j] = array[j+1];
+              array[j+1] = tmp;
+          }
+      }
+   }
 }
 
 void 	insertion_sort (int *array, unsigned long length){
-
+  int i, key, j;
+   for (i = 1; i < length; i++) {
+       key = array[i];
+       j = i-1;
+       while (j >= 0 && array[j] > key) {
+           array[j+1] = array[j];
+           j = j-1;
+       }
+       array[j+1] = key;
+   }
 }
